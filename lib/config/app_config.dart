1aa58516936b2f51fbd36992a83cefedd176c91f
Jules Poyer <jules.poyer@reseau.eseo.fr>
2025-12-15 09:34:22 +0100

    add camera functionality and permissions

---
+++ lib/config/app_config.dart
@@ -0,0 +1,3 @@
+class AppConfig {
+  static const plantNetApiKey = "2b10H92OT9Y0heK6V9CgZgXwW";
+}
---
+++ lib/ui/screens/camera.dart
@@ -0,0 +1,50 @@
+import 'package:camera/camera.dart';
+import 'package:flutter/material.dart';
+
+import '../../main.dart';
+
+class Camera extends StatefulWidget{
+  const Camera({super.key});
+  @override
+  State<Camera> createState() => _CameraState();
+}
+
+class _CameraState extends State<Camera>{
+  late CameraController _controller;
+  late Future<void> _initializeControllerFuture;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = CameraController(
+      cameras.first,
+      ResolutionPreset.high,
+      enableAudio: false,
+    );
+    _initializeControllerFuture = _controller.initialize();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Camera')),
+      body: FutureBuilder<void>(
+        future: _initializeControllerFuture,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.done) {
+            return CameraPreview(_controller);
+          } else {
+            return const Center(child: CircularProgressIndicator());
+          }
+        },
+      ),
+    );
+  }
+  
+}
